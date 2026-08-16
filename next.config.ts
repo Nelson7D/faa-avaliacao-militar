1d3b7a170c39d8ec51b9ed1aac7eaef592621b64
Nelson7D <149388500+Nelson7D@users.noreply.github.com>
2026-08-16 13:51:19 +0100

    feat(config): configurações de build, vitest, firebase e typescript

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
